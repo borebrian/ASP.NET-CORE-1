74bef0fadd4694c5c2754199abd7502085a6f1b6
Bore Brian <borebrian77@gmail.com>
2020-08-13 00:41:55 -0700

    ADDED VIEWBAGS

--- Controllers/CustomeController.cs
+++ Controllers/CustomeController.cs
@@ -3,13 +3,18 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-
+//ADDD THIS FOR VIEWBAG
+using System.Dynamic;
 namespace Fuela_clients.Controllers
 {
     public class CustomeController :Controller
     {
         public ViewResult Index()
         {
+            dynamic data = new ExpandoObject();
+            data.id = 1;
+            data.names = "Brian Kimutai";
+            ViewBag.data = data;
             return View();
         } 
         public ViewResult Aboutus()
